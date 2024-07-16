77d7d1f8e9d56782cf65a089c0e7a612279d7abc
CorneaHoriaMatei <104176032+CorneaHoriaMatei@users.noreply.github.com>
2024-07-16 20:33:10 +0200

    Deleted compilation files

---
+++ .Makefile.d
@@ -0,0 +1,6 @@
+TTtreeDatatypes.vo TTtreeDatatypes.glob TTtreeDatatypes.v.beautified TTtreeDatatypes.required_vo: TTtreeDatatypes.v 
+TTtreeDatatypes.vio: TTtreeDatatypes.v 
+TTtreeDatatypes.vos TTtreeDatatypes.vok TTtreeDatatypes.required_vos: TTtreeDatatypes.v 
+TTtreeProofs.vo TTtreeProofs.glob TTtreeProofs.v.beautified TTtreeProofs.required_vo: TTtreeProofs.v TTtreeDatatypes.vo
+TTtreeProofs.vio: TTtreeProofs.v TTtreeDatatypes.vio
+TTtreeProofs.vos TTtreeProofs.vok TTtreeProofs.required_vos: TTtreeProofs.v TTtreeDatatypes.vos
